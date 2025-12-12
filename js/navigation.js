e65b33a2211d5ecbab7c518211af672251df5937
LeoChavez-DEV <leochavez.dev@hotmail.com>
2025-12-12 17:14:59 +0100

    login funcional

--- js/navigation.js
+++ js/navigation.js
@@ -1,9 +1,17 @@
-function goTo(screen){
-  const map = {
+const routes = {
     home: "../screens/home.html",
-    loader: "../screens/loader.html",
     matcher: "../screens/matcher.html",
-    skills: "../screens/skills.html"
-  };
-  window.location.href = map[screen] || map.home;
-} 
+    skills: "../screens/skills.html",
+    login: "../screens/login.html",
+    register: "../screens/register.html",
+    profile: "../screens/profile.html",
+    settings: "../screens/settings.html"
+};
+
+function goTo(page){
+    if(!routes[page]){
+        console.error("Ruta no definida:", page);
+        return;
+    }
+    window.location.href = routes[page];
+}
